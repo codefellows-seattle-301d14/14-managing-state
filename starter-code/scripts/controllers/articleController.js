66420bafbe3a7b6f0c2e56e6ae132d8799ab0d84
Ted Callahan <CCallahanIV@gmail.com>
2016-11-10 16:27:35 -0800

    completed commenting.

--- starter-code/scripts/controllers/articleController.js
+++ starter-code/scripts/controllers/articleController.js
@@ -12,6 +12,16 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /*loadById takes as parameters the context object (ctx) and the next function to be called in the routing path (in this case is articleController.index) and is a part of the /article/:id route path.
+
+articleData is a callback function used by Article.findWhere.  It sets the articles property of the ctx object equal to an instance of article, and then invokes the next() function (in this case articleController.index).
+
+  Article.findWhere is a method that performs a SQL query given a FIELD (row) and value (in this case the value of the id property of the ctx.params object) and then runs a callback function on the result.
+
+  In summary, this method loads a specific article, by ID.  It uses a SQL query to find the specific article for a given ID and then sets the context property article equal to that article object.
+
+  The next function is articleController.index which will take whatever contents there are of ctx.article and pass them to articleView.Index to be rendered onto the page.*/
+
   articleController.loadById = function(ctx, next) {
     var articleData = function(article) {
       ctx.articles = article;
@@ -21,6 +31,8 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+
+  /*This method functions similarly to loadById, only it loads articles by author.  However, before passing the query (author's name) to findWhere, it removes any + signs from the string. */
   articleController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
@@ -33,6 +45,7 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /*loadByCategory functions similiarly to the above functions, except that it filters by category. */
   articleController.loadByCategory = function(ctx, next) {
     var categoryData = function(articlesInCategory) {
       ctx.articles = articlesInCategory;
@@ -43,6 +56,9 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /* this function effectively loads ALL articles in the ctx.Articles object and passes them to the articleController.index rendering path.
+
+  HOWEVER, it conditionally checks whether or not the Article.allArticles array has any articles loaded into it.  If it does not, it calls Article.fetchAll, passing the articleData callback function, in order to load all the articles from the database.  Article.fetchAll checks to see if the SQL table has been populated.  If it has, it passes the article data to the callback function, if not, it loads the data from the database and creates the webDB sql table, then calls loadAll, and calls the callback function. */
   articleController.loadAll = function(ctx, next) {
     var articleData = function(allArticles) {
       ctx.articles = Article.allArticles;
--- starter-code/scripts/views/articleView.js
+++ starter-code/scripts/views/articleView.js
@@ -16,6 +16,12 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /*Populate filters is invoked by articleView.index, it takes no arguments.  the variable options is set equal to the array of allAuthors over which the .map method is called to use the Handlebars #options-template, given the object {val: author}, effectively stuffing the rendered templates for each author name back into the options array.  Then, it appends the rendered HTML from the options array to #author-filter.
+
+  Next, it calles allCategories method on the Article method and passes an anonymous function with rows as a parameter.  The anonymous function appends to #category-filter the contents of the rows array, after the Handlebars template has been mapped over the contents of rows.
+
+  Article.allCategories uses webDB to select all distinct categories from the SQL databse, and passes that into the callback function described above (becoming the rows array).*/
+
   articleView.populateFilters = function() {
     var options;
     var template = Handlebars.compile($('#option-template').text());
@@ -35,6 +41,17 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+
+  /*  handleFilters is invoked by articleView.index.  It takes no arguments.  It selects the unordered list with the id "filters" and invokes the .one method on it.  .one attaches an event handler to an event for the elements.  The handler is execture at most once per element per event type.  In this case, 'change' and 'select' are the events.  It invokes as a handler an anonymous callback function.
+
+  this represents the selection of the dropdown menu and all of its contents.
+
+  The event handler decelares as a variable resource, which is a string representing whichever dropdown menu is selected.  It strips the '-filter' portion of its id.  (e.g. author-filter becomes author).
+
+  Following declaration for resouce, the val of the sibling list is set to an empty string - clearing that selection.
+
+  Finally, a route set up a url built out of the resource variable (author or category) + a regex expression that replaces all white space with + signs.
+    */
   articleView.handleFilters = function() {
     $('#filters').one('change', 'select', function() {
       var resource = this.id.replace('-filter', '');
@@ -83,7 +100,15 @@
      });
    }; */
 
-  // COMMENT: What does this method do?  What is it's execution path?
+  // COMMENT: What does this method do?  What is it's execution path.
+  /*articleView.index takes as an argument the array of article objects.  It first selects the section with id "articles," shows it, selects the siblings, and hides all of them.
+
+  Next, it finds all article elements in the section with id "article" and removes them. Then, using a forEach method on the array of articles, it appends to the #articles section by calling render on the article object (represented as 'a').
+
+  Next, it calls two methods on the articleView object: populateFilters and handleFilters.
+
+  Finally, a conditional statement tests if there are more than one article.  If true, it will hide a portion of every article body.
+  */
   articleView.index = function(articles) {
     $('#articles').show().siblings().hide();
 
